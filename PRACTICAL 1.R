2cb1ea45cb56b4896c6a7ffd96b94d682d6f525b
aryasasil00179434 <l00179434@atu.ie>
2024-03-14 09:43:21 +0000

    commit

--- PRACTICAL 1.R
+++ PRACTICAL 1.R
@@ -8,3 +8,23 @@ w<-c(2,7,NA,20)
 sum(w)
 v<-w[!is.na(w)]
 v
+
+# Check the type of vector v
+typeof_v <- typeof(v)
+typeof_v 
+
+# Check if each element in x is greater than each element in y
+X > Y
+X<Y
+
+#append function
+
+x1<- append(X,12)
+x1
+
+u<- c(5,6,7,8,9,10)
+u<-append(u,letters[1])
+
+u<-append(u,letters[5:20])
+u
+
